cb265207e1b5b3f2854dc28cd65e0bbe998ea5f3
lingyuzzz <elinazhao8@gmail.com>
2025-04-20 12:29:39 +0800

    unfinished overview

--- frontend/lib/homepage.dart
+++ frontend/lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/task_screen.dart';
 import 'package:frontend/screens/habit_screen.dart';
+import 'package:frontend/screens/overview_screen.dart';
 
 class ExampleDestination {
   const ExampleDestination(this.label, this.icon, this.selectedIcon);
@@ -71,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                 }),
               ],
             ),
-            HabitScreen(),
+            OverviewScreen(),
           ],
         ),
       ),
